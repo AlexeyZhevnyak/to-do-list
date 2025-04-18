efa27281e975367a0feef44dc8d999053ce12b87
Alex Zhevniak <alex.zhevniak@centrica.com>
2025-04-18 10:13:14 +0200

    fix build errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import './App.css'
 import HomePage from "src/pages/home/ui/HomePage.tsx";
+import supabase from "src/api";
+import {useEffect} from "react";
+import {useTodoStore} from "src/entities/todo";
 
 function App() {
+    const addTodos = useTodoStore(state => state.addTodos)
+
+    useEffect(() => {
+        const initData = async () => {
+            try {
+                const {data: items,} = await supabase
+                    .from('items')
+                    .select('*')
+                return items || [];
+            } catch {
+                return []
+            }
+        }
+        initData().then(value => addTodos(value))
+
+
+    }, [addTodos])
     return (
         <div className="app">
             <HomePage/>
--- src/entities/todo/model/store.ts
+++ src/entities/todo/model/store.ts
@@ -1,25 +1,23 @@
 import {create} from 'zustand';
-import supabase from "src/api";
 import {Todo} from "src/entities/todo";
 
 interface TodoStore {
     todos: Todo[];
     addTodo: (text: string) => void;
+    addTodos: (todos: Todo[]) => void;
     toggleTodo: (id: number) => void;
     deleteTodo: (id: number) => void;
 }
 
-const { data: items, } = await supabase
-    .from('items')
-    .select('*')
-
-
 export const useTodoStore = create<TodoStore>((set) => ({
-    todos: items || [],
+    todos: [],
     addTodo: (text) =>
         set((state) => ({
             todos: [...state.todos, {id: Date.now(), text, completed: false, created_at: Date.now().toString()}]
         })),
+    addTodos: (todos) => set((state) => ({
+        todos: [...state.todos, ...todos]
+    })),
     toggleTodo: (id) =>
         set((state) => ({
             todos: state.todos.map((todo) =>
